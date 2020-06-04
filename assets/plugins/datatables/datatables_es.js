e1306539179fd1dafcc90cc6ef01f18bf35ece5e
Yasmani Palmero Valdivia <yasmany.07@gmail.com>
2020-06-04 17:54:04 -0400

    full

---
+++ assets/plugins/datatables/datatables_es.js
@@ -0,0 +1,30 @@
+var esES = {
+  processing: "Procesando...",
+  search: "Buscar:",
+  lengthMenu: "Mostrar _MENU_ registros",
+  info:
+    "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+  infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+  infoFiltered: "(filtrado de un total de _MAX_ registros)",
+  infoPostFix: "",
+  loadingRecords: "Cargando...",
+  zeroRecords: "No se encontraron resultados",
+  emptyTable: "Ningún dato disponible en esta tabla",
+  paginate: {
+    first: "Primero",
+    previous: "Anterior",
+    next: "Siguiente",
+    last: "Último",
+  },
+  aria: {
+    sortAscending: ": Activar para ordenar la columna de manera ascendente",
+    sortDescending: ": Activar para ordenar la columna de manera descendente",
+  },
+  buttons: {
+    copy: "<i class='fa fa-files-o'></i> Copiar",
+    excel: "<i class='fa fa-file-excel-o'></i> Excel",
+    pdf: '<i class="fa fa-file-pdf-o"></i> PDF',
+    colvis: "Visibilidad",
+    print: "<i class='fa fa-print'></i> Imprimir",
+  },
+};
